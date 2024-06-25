3ea406a4d05d98f3c57a4322bb5e7f06948184f5
Gabriela Guayara <gabriela.guayara@gmail.com>
2024-06-24 23:08:13 -0400

    Organized code and added comments to explain the code

--- workingWithAxios/app.js
+++ workingWithAxios/app.js
@@ -5,19 +5,8 @@ const breedSelect = document.getElementById("breedSelect");
 
 const getFavouritesBtn = document.getElementById("getFavouritesBtn");
 
-const getFavourite = document.querySelector('.favourite-button')
 
-// * 4. Change all of your fetch() functions to axios!
-//  * - axios has already been imported for you within index.js.
-//  * - If you've done everything correctly up to this point, this should be simple.
-//  * - If it is not simple, take a moment to re-evaluate your original code.
-//  * - Hint: Axios has the ability to set default headers. Use this to your advantage
-//  *   by setting a default header with your API key so that you do not have to
-//  *   send it manually with all of your requests! You can also set a default base URL!
-//  */
-// /**
-//  * 
-
+// * 4. Change all of your fetch() functions to axios.
 
 axios.defaults.baseURL = "https://api.thecatapi.com/v1/"
 
@@ -43,9 +32,6 @@ async function initialLoad(){
 
         }}
             
-            //Use data from the API to create the options for the dropmenu
-
-
 
 initialLoad()
 
@@ -78,16 +64,9 @@ breedSelect.addEventListener('change', async function () {
 
     }})
       
-        //Use data from the API to create the options for the dropmenu
-
-
-initialLoad()
 
 
 /* 5. Add axios interceptors to log the time between request and response to the console.
- * - Hint: you already have access to code that does this!
- * - Add a console.log statement to indicate when requests begin.
- * - As an added challenge, try to do this on your own without referencing the lesson material.
  */
 
 const instance = axios.create()
@@ -100,22 +79,7 @@ instance.interceptors.request.use((config) => {
 });
 
   /**
- * 6. Next, we'll create a progress bar to indicate the request is in progress.
- * - The progressBar element has already been created for you.
- *  - You need only to modify its "width" style property to align with the request progress.
- * - In your request interceptor, set the width of the progressBar element to 0%.
- *  - This is to reset the progress with each request.
- * - Research the axios onDownloadProgress config option.
- * - Create a function "updateProgress" that receives a ProgressEvent object.
- *  - Pass this function to the axios onDownloadProgress config option in your event handler.
- * - console.log your ProgressEvent object within updateProgess, and familiarize yourself with its structure.
- *  - Update the progress of the request using the properties you are given.
- * - Note that we are not downloading a lot of data, so onDownloadProgress will likely only fire
- *   once or twice per request to this API. This is still a concept worth familiarizing yourself
- *   with for future projects.
- */
-
-  /*
+ * 6. Create a progress bar to indicate the request is in progress.
  * 7. As a final element of progress indication, add the following to your axios interceptors:
  * - In your request interceptor, set the body element's cursor style to "progress."
  * - In your response interceptor, remove the progress cursor style from the body element.
@@ -132,23 +96,9 @@ instance.interceptors.request.use((config) => {
   
 
 
+/** 8.Create a system to "favourite" certain images.*/
 
-
-
-/**
- * 8. To practice posting data, we'll create a system to "favourite" certain images.
- * - The skeleton of this function has already been created for you.
- * - This function is used within Carousel.js to add the event listener as items are created.
- *  - This is why we use the export keyword for this function.
- * - Post to the cat API's favourites endpoint with the given ID.
- * - The API documentation gives examples of this functionality using fetch(); use Axios!
- * - Add additional logic to this function such that if the image is already favourited,
- *   you delete that favourite using the API, giving this function "toggle" functionality.
- * - You can call this function by clicking on the heart at the top right of any image.
- */
-
-const favSection = document.getElementById('favoriteImages')
-
+//Export function to be used in the carousel.js file
 export async function favourite (imgId) {
     try{
     const response = await axios.get(`/favourites?image_id=${imgId}`)
@@ -160,30 +110,29 @@ export async function favourite (imgId) {
     }
   }  
 
-
-// getFavouritesBtn.addEventListener('click', async function(){
-    
-   
-
-
+  /*9. Test your favourite() function by creating a getFavourites() function by using axios to get all of your favourites from the cat API.*/
   
-// })
+getFavouritesBtn.addEventListener('click', async function(){
+ 
+    Carousel.clear();
 
-  /**
-   * 9. Test your favourite() function by creating a getFavourites() function.
-   * - Use Axios to get all of your favourites from the cat API.
-   * - Clear the carousel and display your favourites when the button is clicked.
-   *  - You will have to bind this event listener to getFavouritesBtn yourself.
-   *  - Hint: you already have all of the logic built for building a carousel.
-   *    If that isn't in its own function, maybe it should be so you don't have to
-   *    repeat yourself in this section.
-   */
-  
-  /**
-   * 10. Test your site, thoroughly!
-   * - What happens when you try to load the Malayan breed?
-   *  - If this is working, good job! If not, look for the reason why and fix it!
-   * - Test other breeds as well. Not every breed has the same data available, so
-   *   your code should account for this.
-   */
-  
+    try{
+        const response = await axios.get("/favourites")
+        const data = await response.data
+       
+        data.forEach(favourite =>{
+           let imgSrc = favourite.image.url;
+           let imgAlt = favourite.image.id;
+           let imgId = favourite.image.id;
+           let imgFavourite = Carousel.createCarouselItem(imgSrc, imgAlt, imgId)
+            Carousel.appendCarousel(imgFavourite)
+
+        })
+         console.log(data)
+
+        Carousel.start()
+}catch(e){
+    console.log(e.message)
+}
+})  
+
